5e61a2b66cadd60db843e3a716f10195a20dd4b7
Lenschow19 <55918642+Lenschow19@users.noreply.github.com>
2021-05-03 11:37:40 +0200

    Variable searchbar

--- app/src/main/java/com/easv/tkm/recipeapp/DetailsActivity.kt
+++ app/src/main/java/com/easv/tkm/recipeapp/DetailsActivity.kt
@@ -2,10 +2,18 @@ package com.easv.tkm.recipeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.easv.tkm.recipeapp.data.Models.Recipe
+import kotlinx.android.synthetic.main.activity_details.*
 
 class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+    }
+
+    fun initializeText(recipe: Recipe) {
+        tvTitle.setText(recipe.title)
+        tvDescription.setText(recipe.description)
+        tvPreparations.setText(recipe.preparations)
     }
 }
