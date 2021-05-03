32da12099920a1c1c6de8ae2c107cd44880bfe32
Anders Elis Præstegaard Madsen <ande530c@easv365.dk>
2021-05-03 13:43:45 +0200

    RecyclerAdapter

---
+++ app/src/main/java/com/easv/tkm/recipeapp/data/IntentValues.kt
@@ -0,0 +1,12 @@
+package com.easv.tkm.recipeapp.data
+
+enum class IntentValues(val code: Int) {
+
+    //Requestcodes
+    REQUEST_DETAIL(1),
+
+    //Responsedetails
+    RESPONSE_DETAIL_CREATE(1),
+    RESPONSE_DETAIL_UPDATE(2),
+    RESPONSE_DETAIL_DELETE(3),
+}
