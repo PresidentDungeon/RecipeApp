00f7f8f91493046e7e3b8f47564ecb61e8fbfdce
Anders Elis Præstegaard Madsen <ande530c@easv365.dk>
2021-05-03 13:48:20 +0200

    Activity fix

--- app/src/main/java/com/easv/tkm/recipeapp/GUI/CrudActivity.kt
+++ app/src/main/java/com/easv/tkm/recipeapp/GUI/CrudActivity.kt
@@ -1,7 +1,8 @@
-package com.easv.tkm.recipeapp
+package com.easv.tkm.recipeapp.GUI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.easv.tkm.recipeapp.R
 
 class CrudActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
