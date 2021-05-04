6a637f0cd57a3e452d0adc93274875961e298f58
Jackei94 <55141365+Jackei94@users.noreply.github.com>
2021-05-04 13:43:07 +0200

    Working on SMS

--- app/src/main/java/com/easv/tkm/recipeapp/GUI/DetailsActivity.kt
+++ app/src/main/java/com/easv/tkm/recipeapp/GUI/DetailsActivity.kt
@@ -1,23 +1,26 @@
 package com.easv.tkm.recipeapp.GUI
 
+import android.content.Intent
 import android.content.res.TypedArray
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Debug
 import android.util.Log
+import android.view.View
 import android.widget.ArrayAdapter
 import com.easv.tkm.recipeapp.DAL.RecipeRepository
 import com.easv.tkm.recipeapp.R
 import com.easv.tkm.recipeapp.data.Models.Category
 import com.easv.tkm.recipeapp.data.Models.IngredientEntry
 import com.easv.tkm.recipeapp.data.Models.Recipe
-import kotlinx.android.synthetic.main.activity_crud.*
 import kotlinx.android.synthetic.main.activity_details.*
 import kotlinx.android.synthetic.main.activity_details.tvDescription
 import kotlinx.android.synthetic.main.activity_details.tvPreparations
 import kotlinx.android.synthetic.main.activity_details.tvTitle
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
+import java.io.File
 
 class DetailsActivity : AppCompatActivity() {
 
@@ -32,13 +35,41 @@ class DetailsActivity : AppCompatActivity() {
         this.recipe = intent.extras?.getSerializable("RECIPE") as Recipe
         this.ingredients = intent.extras?.getSerializable("INGREDIENTS") as Array<IngredientEntry>
 
-        //Should run initialize here
-
+        initializeText()
+        var file: File = File(this.recipe.imageURL)
+        if (file!!.exists()){ivImage.setImageURI(Uri.fromFile(file))}
+        else{ivImage.setImageResource(R.drawable.placeholder)}
+        updateIngredients()
     }
 
-    fun initializeText(recipe: Recipe) {
+    fun initializeText() {
         tvTitle.setText(recipe.title)
         tvDescription.setText(recipe.description)
         tvPreparations.setText(recipe.preparations)
     }
+
+    private fun updateIngredients() {
+        var ingredientString = ""
+
+        ingredients.forEach{ ingredient -> ingredientString += "${ingredient.name}: ${ingredient.amount} ${ingredient.measurementUnit} \n" }
+        tvIngredients.setText(ingredientString)
+    }
+
+    fun share(view: View) {
+        val sendIntent = Intent(Intent.ACTION_VIEW,Uri.parse("sms:"))
+        //sendIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(File(recipe.imageURL)))
+        //sendIntent.type = "image/gif"
+        //sendIntent.data = Uri.parse("")
+        sendIntent.putExtra("sms_body", "Testing")
+        startActivity(sendIntent)
+    }
+
+    private fun preparedMMS(): String {
+        var MMS = ""
+
+        MMS += "${recipe.title} \n ${recipe.description} \n"
+
+        return MMS
+    }
+
 }
