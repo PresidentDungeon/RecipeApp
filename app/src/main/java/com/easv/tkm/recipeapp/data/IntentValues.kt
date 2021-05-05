637df5ad809e34f07091980bd6bfbe2f36d73406
Anders Elis Præstegaard Madsen <ande530c@easv365.dk>
2021-05-05 11:08:02 +0200

    Start image select pick

--- app/src/main/java/com/easv/tkm/recipeapp/data/IntentValues.kt
+++ app/src/main/java/com/easv/tkm/recipeapp/data/IntentValues.kt
@@ -6,7 +6,9 @@ enum class IntentValues(val code: Int) {
     REQUEST_DETAIL(1),
     REQUESTCODE_IMAGE_APP(2),
     REQUESTCODE_IMAGE_DIRECT(3),
-    REQUESTCODE_CRUD(4),
+    REQUESTCODE_IMAGE_PICK(4),
+    REQUESTCODE_CRUD(5),
+
 
     //Responsedetails
     RESPONSE_DETAIL_CREATE(1),
